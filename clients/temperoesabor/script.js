fc4f039fcc24255d275d7a761e9bcc645e2fde79
Frank <frank_diogo@hotmail.com>
2025-11-13 20:02:44 -0300

    Corrige caminhos das imagens para assets/images e adiciona todas as imagens dos produtos

--- clients/temperoesabor/script.js
+++ clients/temperoesabor/script.js
@@ -14,7 +14,7 @@
 const CONFIG = {
     restaurantName: 'Tempero & Sabor',
     whatsappNumber: '67982077085', // Format: DDD + number (no spaces or special chars)
-    logoPath: '../../../Clientes/Tempero&Sabor/TemperoESaborLogo.png',
+    logoPath: '../../assets/images/TemperoESaborLogo.png',
     restaurantLatitude: -20.367082707152765,
     restaurantLongitude: -51.42205139592757,
     openingHours: {
@@ -38,7 +38,7 @@ const MENU_DATA = {
             description: 'Pão, hambúrguer de carne, queijo mussarela, bacon e molho mima',
             price: 22.00,
             category: 'Lanches',
-            image: '../../../Clientes/Tempero&Sabor/Imagens Produtos/Burgers 01 - voMima.png'
+            image: '../../assets/images/Burgers 01 - voMima.png'
         },
         {
             id: '2',
@@ -46,7 +46,7 @@ const MENU_DATA = {
             description: 'Pão, hambúrguer de carne, queijo mussarela, bacon, ovo, calabresa, alface, tomate e molho mima',
             price: 28.00,
             category: 'Lanches',
-            image: '../../../Clientes/Tempero&Sabor/Imagens Produtos/Burgers 02 - voLeleu.png'
+            image: '../../assets/images/Burgers 02 - voLeleu.png'
         },
         {
             id: '3',
@@ -54,7 +54,7 @@ const MENU_DATA = {
             description: 'Pão, hambúrguer de carne, queijo mussarela, bacon, alface, tomate e molho barbecue',
             price: 24.00,
             category: 'Lanches',
-            image: '../../../Clientes/Tempero&Sabor/Imagens Produtos/Burgers 03 - voAna.png'
+            image: '../../assets/images/Burgers 03 - voAna.png'
         },
         {
             id: '4',
@@ -62,7 +62,7 @@ const MENU_DATA = {
             description: 'Pão, hambúrguer de carne, queijo mussarela, queijo cheddar, bacon, alface, tomate e molho barbecue',
             price: 27.00,
             category: 'Lanches',
-            image: '../../../Clientes/Tempero&Sabor/Imagens Produtos/Burgers 04 - tiaTa.png'
+            image: '../../assets/images/Burgers 04 - tiaTa.png'
         },
         {
             id: '5',
@@ -70,7 +70,7 @@ const MENU_DATA = {
             description: 'Pão, hambúrguer de frango, queijo cheddar, bacon, alface, tomate e molho mima',
             price: 24.00,
             category: 'Lanches',
-            image: '../../../Clientes/Tempero&Sabor/Imagens Produtos/Burgers 05 - tioJe.png'
+            image: '../../assets/images/Burgers 05 - tioJe.png'
         },
         {
             id: '6',
@@ -78,7 +78,7 @@ const MENU_DATA = {
             description: 'Pão, hambúrguer de picanha, queijo cheddar, bacon e molho mima',
             price: 38.00,
             category: 'Lanches',
-            image: '../../../Clientes/Tempero&Sabor/Imagens Produtos/Burgers 06 - tioLan.png'
+            image: '../../assets/images/Burgers 06 - tioLan.png'
         },
         {
             id: '7',
@@ -86,7 +86,7 @@ const MENU_DATA = {
             description: 'Pão, 3 hambúrgueres de carne, camadas de queijo mussarela, queijo cheddar, bacon e molho mima',
             price: 43.00,
             category: 'Lanches',
-            image: '../../../Clientes/Tempero&Sabor/Imagens Produtos/Burgers 07 - tiaPri.png'
+            image: '../../assets/images/Burgers 07 - tiaPri.png'
         },
         {
             id: '8',
@@ -94,7 +94,7 @@ const MENU_DATA = {
             description: 'Pão, hambúrguer de pernil, queijo mussarela, bacon, alface, tomate e molho barbecue',
             price: 25.00,
             category: 'Lanches',
-            image: '../../../Clientes/Tempero&Sabor/Imagens Produtos/Burgers 08 - tiaTay.png'
+            image: '../../assets/images/Burgers 08 - tiaTay.png'
         },
         {
             id: '9',
@@ -102,7 +102,7 @@ const MENU_DATA = {
             description: 'Pão, hambúrguer de carne, queijo mussarela, bacon, ovo, cebola caramelizada, alface, tomate e molho mima',
             price: 28.00,
             category: 'Lanches',
-            image: '../../../Clientes/Tempero&Sabor/Imagens Produtos/Burgers 09 - voLio.png'
+            image: '../../assets/images/Burgers 09 - voLio.png'
         },
         {
             id: '10',
@@ -110,7 +110,7 @@ const MENU_DATA = {
             description: 'Pão brioche, hambúrguer de carne, queijo mussarela e molho mima',
             price: 20.00,
             category: 'Lanches',
-            image: '../../../Clientes/Tempero&Sabor/Imagens Produtos/Burgers 10 - voNini.png'
+            image: '../../assets/images/Burgers 10 - voNini.png'
         },
         {
             id: '11',
@@ -118,7 +118,7 @@ const MENU_DATA = {
             description: 'Pão, hambúrguer de carne, queijo mussarela, tomate, alface, cebola roxa e molho mima',
             price: 25.00,
             category: 'Lanches',
-            image: '../../../Clientes/Tempero&Sabor/Imagens Produtos/Burgers 11 - voSolis.png'
+            image: '../../assets/images/Burgers 11 - voSolis.png'
         },
         {
             id: '12',
@@ -126,7 +126,7 @@ const MENU_DATA = {
             description: 'Pão, hambúrguer de carne, queijo mussarela, alface, tomate, cebola roxa, picles e molho mima',
             price: 27.00,
             category: 'Lanches',
-            image: '../../../Clientes/Tempero&Sabor/Imagens Produtos/Burgers 12 - voNilton.png'
+            image: '../../assets/images/Burgers 12 - voNilton.png'
         },
         {
             id: '13',
@@ -134,7 +134,7 @@ const MENU_DATA = {
             description: 'Pão, duplo hambúrguer de carne, duplo queijo mussarela, onion rings e molho barbecue',
             price: 33.00,
             category: 'Lanches',
-            image: '../../../Clientes/Tempero&Sabor/Imagens Produtos/Burgers 13 - gemeasLela.png'
+            image: '../../assets/images/Burgers 13 - gemeasLela.png'
         }
     ]
 };
